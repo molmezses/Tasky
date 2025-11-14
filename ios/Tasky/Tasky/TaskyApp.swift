6ae5f1a8af707bcc6a906aa7e47cb57ba1f59c5c
molmezses <mustafaolmezses@gmail.com>
2025-11-14 21:33:32 +0300

    installed the ios project

---
+++ ios/Tasky/Tasky/TaskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskyApp.swift
+//  Tasky
+//
+//  Created by Mustafa Ölmezses on 14.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
